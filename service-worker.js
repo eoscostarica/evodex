c21c074d45b79a0a3110c878bd245da6962af059
tetogomez <tetogomez@users.noreply.github.com>
2020-08-20 02:56:00 +0000

    deploy: 3cd142af7fd8175f5bbda24bdd8eb61d3e0cdb86

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d910ec5dc54aaec7228095755b58f2ac.js"
+  "/precache-manifest.9906048ea51b2a30446be157d076535c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
