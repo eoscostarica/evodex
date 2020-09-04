655035dec80342a9356a365f9c2529416d585e22
Teto Gomez <terencio17@rocketmail.com>
2020-09-03 18:31:23 -0600

    auto code review

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -103,16 +103,19 @@ const TableData = ({ data, onClick }) => {
 
   const stableSort = (array, comparator) => {
     const stabilizedThis = array.map((el, index) => [el, index])
+
     stabilizedThis.sort((a, b) => {
       const order = comparator(a[0], b[0])
       if (order !== 0) return order
       return a[1] - b[1]
     })
+
     return stabilizedThis.map((el) => el[0])
   }
 
   const handleRequestSort = (property) => {
     const isAsc = orderBy === property && order === 'asc'
+    
     setOrder(isAsc ? 'desc' : 'asc')
     setOrderBy(property)
   }
