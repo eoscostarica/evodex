309df86371d214e41037e684ae14a13198de4287
xavier506 <xavier506@users.noreply.github.com>
2020-10-07 20:27:37 +0000

    deploy: 754fdeb6c49caa0f223f1b2c97bdc378faa9988c

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf5811a1c52b0184e09869d0748855f2.js"
+  "/precache-manifest.44a291369298d9af5e92d90eab387bb6.js"
 );
 
 self.addEventListener('message', (event) => {
